31e2183eb962d30d068fe14d4b60d92fecc09662
crisischris <christopherthomasnelson@gmail.com>
2021-01-25 20:23:04 -0800

    add granular sensitive radar

--- Assets/game_Asteroids/scripts/asteroid.cs
+++ Assets/game_Asteroids/scripts/asteroid.cs
@@ -203,7 +203,7 @@ public class Asteroid : MonoBehaviour
 
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, mask))
         {
-            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
             //Debug.Log(hit.transform.name);
 
             //TODO
--- Assets/game_Asteroids/scripts/ui_manager.cs
+++ Assets/game_Asteroids/scripts/ui_manager.cs
@@ -218,83 +218,164 @@ public class UI_manager : MonoBehaviour
     }
 
 
+    //This function attempts to resize the radar in a device agnostic way.
+    //the basic assumption is 16:9 but should extend to others (18:9)
+    private int[] CalculateScreenRatios()
+    {
+        //ret[0] == top padding
+        //ret[1] == bottom padding
+        //ret[2] == side padding
+        //ret[3] == spacing between radar bars
+        //ret[4] == side bar y value
+        //ret[5] == top / bottom bar x value
+        int[] ret = new int[6];
+
+        int sHeight = Screen.height;
+        int sWidth = Screen.width;
+
+        //account for the stupid bar on screen at the top of the mobile phones 
+        int topPadding = sHeight - (sHeight / 14);
+        int bottomPadding = sHeight / 16;
+        int sidePadding = sWidth / 12;
+        int spacing = topPadding / 14;
+
+        //overall height minus 7x spaces / 8 bars
+        int sideBarSize = (topPadding - (spacing * 7)) / 8;
+
+        //get width between side spaces - 3 spaces in between 4 bars / 4
+        int topBottomBarSize = ((sWidth - sidePadding * 2) - spacing * 3) / 4;
+
+        ret[0] = topPadding;
+        ret[1] = bottomPadding;
+        ret[2] = sidePadding;
+        ret[3] = spacing;
+        ret[4] = sideBarSize;
+        ret[5] = topBottomBarSize;
+
+        return ret;
+    }
     //This function sets up the radar. It has a lot of manual setup but should extent
     //to all mobile devices nicely
     private void SetupRadar()
     {
+        //structure of the return in the CalcScreen function
+        //ret[0] == top padding
+        //ret[1] == bottom padding
+        //ret[2] == side padding
+        //ret[3] == spacing between radar bars
+        //ret[4] == side bar y value
+        //ret[5] == top / bottom bar x value
+        int [] screenValues = CalculateScreenRatios();
+
+
 
         Radar_UI.SetActive(true);
 
         //Set up the front radar chunk
         //note, special find syntax to find a nested gameobject by namae
         GameObject F1 = GameObject.Find("F1_UI");
-        F1.transform.position = new Vector2(Screen.width / 2 - distance_apart, Screen.height - top__bottom_padding);
+        //position the bar at side padding + half bar length, top padding
+        F1.transform.position = new Vector2(screenValues[2] + screenValues[5] / 2, screenValues[0]);
         radars.Add(F1.gameObject);
 
-
         GameObject F2 = GameObject.Find("F2_UI");
-        F2.transform.position = new Vector2(Screen.width / 2, Screen.height - top__bottom_padding);
+        F2.transform.position = new Vector2(F1.transform.position.x + screenValues[3] + screenValues[5], screenValues[0]);
         radars.Add(F2);
 
         GameObject F3 = GameObject.Find("F3_UI");
-        F3.transform.position = new Vector2(Screen.width / 2 + distance_apart, Screen.height - top__bottom_padding);
+        F3.transform.position = new Vector2(F2.transform.position.x + screenValues[3] + screenValues[5], screenValues[0]);
         radars.Add(F3);
 
+        GameObject F4 = GameObject.Find("F4_UI");
+        F4.transform.position = new Vector2(F3.transform.position.x + screenValues[3] + screenValues[5], screenValues[0]);
+        radars.Add(F4);
 
         //Set up the right radar chunk
         GameObject R1 = GameObject.Find("R1_UI");
+        R1.transform.position = new Vector2(Screen.width - screenValues[2] / 2, screenValues[0] - screenValues[4]);
         radars.Add(R1);
-        R1.transform.position = new Vector2(Screen.width - side_padding, Screen.height / 2 + distance_apart*3);
 
+        
         GameObject R2 = GameObject.Find("R2_UI");
-        R2.transform.position = new Vector2(Screen.width - side_padding, Screen.height / 2 + distance_apart);
+        R2.transform.position = new Vector2(Screen.width - screenValues[2] / 2, R1.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
         radars.Add(R2);
 
+        
         GameObject R3 = GameObject.Find("R3_UI");
-        R3.transform.position = new Vector2(Screen.width - side_padding, Screen.height / 2);
+        R3.transform.position = new Vector2(Screen.width - screenValues[2] / 2, R2.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
         radars.Add(R3);
 
         GameObject R4 = GameObject.Find("R4_UI");
-        R4.transform.position = new Vector2(Screen.width - side_padding, Screen.height / 2 - distance_apart);
+        R4.transform.position = new Vector2(Screen.width - screenValues[2] / 2, R3.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
         radars.Add(R4);
 
         GameObject R5 = GameObject.Find("R5_UI");
-        R5.transform.position = new Vector2(Screen.width - side_padding, Screen.height / 2 - distance_apart * 3);
+        R5.transform.position = new Vector2(Screen.width - screenValues[2] / 2, R4.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
         radars.Add(R5);
 
+        GameObject R6 = GameObject.Find("R6_UI");
+        R6.transform.position = new Vector2(Screen.width - screenValues[2] / 2, R5.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
+        radars.Add(R6);
 
+        GameObject R7 = GameObject.Find("R7_UI");
+        R7.transform.position = new Vector2(Screen.width - screenValues[2] / 2, R6.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
+        radars.Add(R7);
+
+        GameObject R8 = GameObject.Find("R8_UI");
+        R8.transform.position = new Vector2(Screen.width - screenValues[2] / 2, R7.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
+        radars.Add(R8);
+
+
+        
         //Set up the back radar chunk
         GameObject B1 = GameObject.Find("B1_UI");
-        B1.transform.position = new Vector2(Screen.width / 2 - distance_apart, top__bottom_padding);
+        B1.transform.position = new Vector2(screenValues[2] + screenValues[5] / 2, screenValues[1]);
         radars.Add(B1);
 
         GameObject B2 = GameObject.Find("B2_UI");
-        B2.transform.position = new Vector2(Screen.width / 2, top__bottom_padding);
+        B2.transform.position = new Vector2(B1.transform.position.x + screenValues[3] + screenValues[5], screenValues[1]);
         radars.Add(B2);
 
         GameObject B3 = GameObject.Find("B3_UI");
-        B3.transform.position = new Vector2(Screen.width / 2 + distance_apart, top__bottom_padding);
+        B3.transform.position = new Vector2(B2.transform.position.x + screenValues[3] + screenValues[5], screenValues[1]);
         radars.Add(B3);
 
-        //Set up the left radar chunk
+        GameObject B4 = GameObject.Find("B4_UI");
+        B4.transform.position = new Vector2(B3.transform.position.x + screenValues[3] + screenValues[5], screenValues[1]);
+        radars.Add(B4);
+
+
+        //Set up the right radar chunk
         GameObject L1 = GameObject.Find("L1_UI");
-        L1.transform.position = new Vector2(side_padding * 2, Screen.height / 2 + distance_apart * 3);
+        L1.transform.position = new Vector2(screenValues[2] / 2, screenValues[0] - screenValues[4]);
         radars.Add(L1);
 
         GameObject L2 = GameObject.Find("L2_UI");
-        L2.transform.position = new Vector2(side_padding, Screen.height / 2 + distance_apart);
+        L2.transform.position = new Vector2(screenValues[2] / 2, L1.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
         radars.Add(L2);
 
         GameObject L3 = GameObject.Find("L3_UI");
-        L3.transform.position = new Vector2(side_padding, Screen.height / 2);
+        L3.transform.position = new Vector2(screenValues[2] / 2, L2.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
         radars.Add(L3);
 
         GameObject L4 = GameObject.Find("L4_UI");
-        L4.transform.position = new Vector2(side_padding, Screen.height / 2 - distance_apart);
+        L4.transform.position = new Vector2(screenValues[2] / 2, L3.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
         radars.Add(L4);
 
         GameObject L5 = GameObject.Find("L5_UI");
-        L5.transform.position = new Vector2(side_padding * 2, Screen.height / 2 - distance_apart * 3);
+        L5.transform.position = new Vector2(screenValues[2] / 2, L4.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
         radars.Add(L5);
+
+        GameObject L6 = GameObject.Find("L6_UI");
+        L6.transform.position = new Vector2(screenValues[2] / 2, L5.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
+        radars.Add(L6);
+
+        GameObject L7 = GameObject.Find("L7_UI");
+        L7.transform.position = new Vector2(screenValues[2] / 2, L6.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
+        radars.Add(L7);
+
+        GameObject L8 = GameObject.Find("L8_UI");
+        L8.transform.position = new Vector2(screenValues[2] / 2, L7.transform.position.y - screenValues[3] - screenValues[4] / 1.5f);
+        radars.Add(L8);
     }
 }
