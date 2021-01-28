db361fc5fbae97dd667db159cd483e88af59c499
crisischris <christopherthomasnelson@gmail.com>
2021-01-27 21:38:45 -0800

    Add launch screen navigation functionality

---
+++ Assets/Launch_screen/scripts/UI_manager_launch.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_manager_launch : MonoBehaviour
+{
+    //this object should be shared across all Scenes
+    public GameObject DoNotDestroy;
+
+
+    public Text highScoreTextAsteroids;
+    public Text highScoreTextPong;
+
+    private AudioSource source;
+    public AudioClip buttonClick;
+
+
+    private int hs_Asteroids = 0, hs_Pong = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //DoNotDestroy = GameObject.Find("DontDestroyOnLoad");
+        DoNotDestroy = GameObject.Find("DoNotDestroy");
+        source = GameObject.Find("Manager_Audio").GetComponent<AudioSource>();
+        //query the existing HS on the device if available, defaul = 0;
+        hs_Asteroids = PlayerPrefs.GetInt("Asteroids_high_score", 0);
+        hs_Pong = PlayerPrefs.GetInt("Pong_high_score", 0);
+
+        highScoreTextAsteroids.text = "high score: " + hs_Asteroids.ToString();
+        highScoreTextPong.text = "high score: " + hs_Pong.ToString();
+
+        highScoreTextAsteroids.transform.position = new Vector2(Screen.width / 2, 2 * (Screen.height / 10));
+        highScoreTextPong.transform.position = new Vector2(Screen.width / 2, highScoreTextAsteroids.transform.position.y - Screen.height/20);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        int a = SceneManager.sceneCountInBuildSettings;
+        Debug.Log(a);
+        //int l = s.Length;
+
+        //for (int i = 0; i < l; i++)
+        //    Debug.Log(s[i].name);
+
+        //Debug.Log(SceneManager.GetActiveScene().name);
+        //Debug.Log(SceneManager.GetAllScenes())
+    }
+
+    public void PlayAsteroids()
+    {
+        DoNotDestroy.GetComponent<DoNotDestroy>().PlayButtonUI();
+        //DoNotDestroy.GetComponentInChildren<DoNotDestroy>().PlayButtonUI();
+        SceneManager.LoadScene("Asteroids");
+    }
+
+    public void PlayPong()
+    {
+        DoNotDestroy.GetComponentInChildren<DoNotDestroy>().PlayButtonUI();
+        SceneManager.LoadScene("Pong");
+    }
+}
---
+++ Assets/Shared_Scripts/DoNotDestroy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DoNotDestroy : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip[] clips;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        source = GetComponent<AudioSource>();
+        source.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+        //asteroids bg music
+        if (sceneName == "Asteroids" && source.clip.name != "Asteroids")
+            PlayAsteroidsTheme();
+        else if (sceneName == "Pong" && source.clip.name != "Pong")
+            PlayPongTheme();
+
+        else if(sceneName == "Launch_screen" && source.clip.name != "Launch")
+            PlayLaunchTheme();
+    }
+
+    public void PlayButtonUI()
+    {
+        source.PlayOneShot(clips[2]);
+    }
+
+    private void PlayAsteroidsTheme()
+    {
+        source.clip = clips[1];
+        source.Play();
+    }
+
+    private void PlayPongTheme()
+    {
+        source.clip = clips[1];
+        source.Play();
+    }
+
+    private void PlayLaunchTheme()
+    {
+        source.clip = clips[0];
+        source.Play();
+    }
+
+}
