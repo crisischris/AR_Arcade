f6b617865d1354cf2c151d82bfea893a75e0dd8a
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2021-01-30 11:43:48 -0600

    createds Player Controller script

---
+++ Assets/game_Pong/Scripts/Player_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public Camera playerView;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount != 1)
+            return;
+
+        var ray = playerView.ScreenPointToRay(Input.touches[0].position);
+        var hitInfo = new RaycastHit();
+        if(Physics.Raycast(ray, out hitInfo))
+        {
+            if (hitInfo.transform.name != "Player Score Wall")
+                return;
+            var movePaddle = transform.position;
+            movePaddle.x = hitInfo.point.x;
+            transform.position = movePaddle;
+        }
+    }
+}
