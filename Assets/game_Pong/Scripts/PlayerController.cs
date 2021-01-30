94a0387e43b4647f6925a858679a500192c71ab8
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2021-01-30 10:29:06 -0600

    ready for build
    slowed down ball and adjust scale for AR

---
+++ Assets/game_Pong/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+public class PlayerController : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
