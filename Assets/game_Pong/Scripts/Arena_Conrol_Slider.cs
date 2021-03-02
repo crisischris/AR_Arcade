5a738d77334f2943260a9bd6f22f3880cda6db7c
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2021-03-02 12:48:55 -0600

    Slider functional, needs some tweaking

---
+++ Assets/game_Pong/Scripts/Arena_Conrol_Slider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Arena_Conrol_Slider : MonoBehaviour
+{
+    public Slider slider;
+    float SliderVal;
+    // Update is called once per frame
+    void Update()
+    {
+        SliderVal = slider.value;
+        Vector3 moveArena = transform.position;
+        moveArena.y = SliderVal;
+        transform.position = moveArena;
+        
+    }
+}
