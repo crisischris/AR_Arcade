528c96ef4670d45508c7c9854222653cd15493b6
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2021-03-02 17:07:00 -0600

    functional;

---
+++ Assets/game_Pong/Scripts/Player_Controller_Slider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Controller_Slider : MonoBehaviour
+{
+    public Slider PlayerSlider;
+    float SliderVal;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        SliderVal = PlayerSlider.value;
+        SliderVal *= -1;
+        Vector3 movePlayer = transform.position;
+        movePlayer.x = SliderVal;
+        transform.position = movePlayer;
+    }
+}
